90114d8c53e1f95d36ddedaa00bd40aaa4a4570b
Michael Henry <drmikehenry@drmikehenry.com>
2012-05-05 07:23:24 -0400

    Added patchutils.

--- cygmin.py
+++ cygmin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim:set fileencoding=utf8: #
 
-__VERSION__ = "0.1.2"
+__VERSION__ = "0.2.0"
 
 README = """
 
@@ -111,6 +111,7 @@ DEFAULT_EXTRA_PACKAGES = """
     ncurses
     netcat
     openssh
+    patchutils
     perl
     python
     rsync
